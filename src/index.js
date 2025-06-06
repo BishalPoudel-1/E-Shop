dd761d8f225f7287d164568044fd96bced49fe71
Bishal Poudel <120388416+BishalPoudel-1@users.noreply.github.com>
2025-06-06 23:52:20 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-    <App />
+    <App /> //start the web app from this line of code.
     </AuthContextProvider>
   </React.StrictMode>
 );
